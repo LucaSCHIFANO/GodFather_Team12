a0b34bc09a27e1bb371b1881e1882f8c6e2be1ae
Chloe-Ms <mllechloe.mathias@gmail.com>
2022-09-06 17:19:26 +0200

    Joueur accroupi

--- Assets/Chloe/Script/PlayerController.cs
+++ Assets/Chloe/Script/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Player player;
     [SerializeField] private Rigidbody2D  rb;
+    [SerializeField] private BoxCollider2D  col;
+    [SerializeField] private Sprite spriteCrouch;
+    [SerializeField] private Sprite sprite;
 
     [SerializeField] private float jumpForce;
     [SerializeField] private int playerID = 0;
@@ -25,16 +28,23 @@ public class PlayerController : MonoBehaviour
             isJumping = true;
         }
         
-        // if (player.GetButtonDown("Crouch")){
-            
-        // }
+        if (player.GetButtonDown("Crouch")){
+            col.offset = new Vector2(0,-0.25f);
+            col.size = new Vector2(1,0.5f);
+            GetComponent<SpriteRenderer>().sprite = spriteCrouch;
+        }
+
+        if (player.GetButtonUp("Crouch")){
+            col.offset = new Vector2(0,0f);
+            col.size = new Vector2(1,1f);
+            GetComponent<SpriteRenderer>().sprite = sprite;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D col){
         if (col.gameObject.tag == "Ground" && isJumping){
             isJumping = false;
         }
-        Debug.Log("OnCollisionEnter2D");
     }
 
 }
