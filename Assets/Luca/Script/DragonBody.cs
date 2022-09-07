c1119b4981e3135ead741ef3703e843fa943fbda
LucaSchifano <luca.schifano005@gmail.com>
2022-09-07 15:16:31 +0200

    Dragon rrrraaahhhh

--- Assets/Luca/Script/DragonBody.cs
+++ Assets/Luca/Script/DragonBody.cs
@@ -5,6 +5,21 @@ using UnityEngine;
 
 public class DragonBody : MonoBehaviour
 {
+    [SerializeField] private float id;
+    private Animator anim;
+    private void Awake()
+    {
+        //anim.enabled = false;
+        anim = GetComponent<Animator>();
+        StartCoroutine(StartAnim());
+    }
+
+    IEnumerator StartAnim()
+    {
+        yield return new WaitForSeconds(0.2f * id);
+        anim.enabled = true;
+    }
+    
     /*[SerializeField] private Transform parent;
     [SerializeField] private float speed;
     [SerializeField] private float maxSpeed;
