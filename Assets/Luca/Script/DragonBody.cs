e5ad66311583ca64c2a7a60e45e94fd5085d5744
LucaSchifano <luca.schifano005@gmail.com>
2022-09-07 15:54:22 +0200

    Delete obstacles after time

--- Assets/Luca/Script/DragonBody.cs
+++ Assets/Luca/Script/DragonBody.cs
@@ -20,27 +20,4 @@ public class DragonBody : MonoBehaviour
         anim.enabled = true;
     }
     
-    /*[SerializeField] private Transform parent;
-    [SerializeField] private float speed;
-    [SerializeField] private float maxSpeed;
-    private float actualSpeed;
-    private Rigidbody2D rb;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-    
-    private void Update()
-    {
-        if(transform)
-        
-        
-    }
-
-    private void FixedUpdate()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, actualSpeed);
-        transform.position = new Vector2(parent.position.x, transform.position.y);
-    }*/
 }
