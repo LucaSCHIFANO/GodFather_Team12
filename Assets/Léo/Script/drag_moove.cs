af7368481bd2eeffe5e42fdb17eb71e2336c6019
AmiraleBjorn <112935129+AmiraleBjorn@users.noreply.github.com>
2022-09-07 09:51:32 +0200

    Shoot

--- Assets/Léo/Script/drag_moove.cs
+++ Assets/Léo/Script/drag_moove.cs
@@ -5,8 +5,9 @@ using Rewired;
 
 public class drag_moove : MonoBehaviour
 {
-    public projectile Projectile;
-
+    public projectile projectilePrefab;
+    public Transform LaunchOffSet;
+ 
     private Rigidbody2D rigidbody;
 
     [SerializeField] private float speed;
@@ -15,17 +16,31 @@ public class drag_moove : MonoBehaviour
     [SerializeField]private int playerID = 0 ;
     [SerializeField]private Player player;
 
+    private void Awake()
+    {
+        
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
 
-     void Start()
+    void Start()
     {
         player = ReInput.players.GetPlayer(playerID);
-        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
         float moveVertical = player.GetAxis("Vertical");
+        float moveHorizontal = player.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(moveHorizontal* speed, moveVertical* speed);
 
-        rigidbody.velocity = new Vector2(0, moveVertical);
+
+        
+    }
+    private void Update()
+    {
+        if (player.GetButtonDown("Shoot"))
+        {
+            Instantiate(projectilePrefab, LaunchOffSet.position, transform.rotation);
+        }
     }
 }
