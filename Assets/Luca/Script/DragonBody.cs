9e1a9bc966e4e2f52a872bfe10a021d657b93f48
LucaSchifano <luca.schifano005@gmail.com>
2022-09-07 14:42:33 +0200

    Dragon body

---
+++ Assets/Luca/Script/DragonBody.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonBody : MonoBehaviour
+{
+    /*[SerializeField] private Transform parent;
+    [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
+    private float actualSpeed;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    private void Update()
+    {
+        if(transform)
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, actualSpeed);
+        transform.position = new Vector2(parent.position.x, transform.position.y);
+    }*/
+}
